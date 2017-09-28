9a69602731246387e4613f68a06d89e68c59c7c1
chengyakun11 <chengyakun11@163.com>
2017-09-28 14:37:42 +0800

    Initial Commit

---
+++ MessCodeSnippet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MessCodeSnippet
+//
+//  Created by zhangxun on 2017/9/28.
+//  Copyright © 2017年 fudianshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
